44eb33a7254b0cdad719c4519f40280cc9f1b0cc
AntonUsov <47754735+AntonUsov@users.noreply.github.com>
2019-02-19 01:12:04 +0700

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,7 +1,6 @@
 import collections
 import re
 class parser():
-
 	def open_file(self, src):
 		log = open(src, 'r').read() 
 		user_list = re.findall(r'user\w+', log)
@@ -11,11 +10,7 @@ class parser():
 			t.append({'user': key, 'commit': value})
 		for elem in t:
 			print('user: ' + elem['user'] + ' - ' + 'commit: ' + str(elem['commit']) )
-		return 
-
 if __name__ == "__main__":
 	parser1=parser()
 	src = raw_input('Vvedite pyt: ')
-
-	print(src);
-	print(parser1.open_file(src));
+	parser1.open_file(src);
